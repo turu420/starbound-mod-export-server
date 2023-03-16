16f186f4a4374aff0c9a359ed2fe979ff2a22867
turu <turushimizu@gmail.com>
2023-03-16 23:02:45 +0100

    skript done

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
-# starbound-mod-export-server
- Python Skript to Export Mods to Server Mods
+# HOW TO
+
+1. Install any Python 3 Version
+2. copy "exportMods.py" into "SteamLibrary\steamapps\workshop\content\211820"
+3. create "export" Folder in "SteamLibrary\steamapps\workshop\content\211820"
+4. Run CMD inside "SteamLibrary\steamapps\workshop\content\211820"
+5. run "python exportMods.py"
+6. Wait
+7. Your renamed mods will be in export
+
+
+Hope I saved your time <3
+
+# In server
+
+just place them into the server folder
---
+++ exportMods.py
@@ -0,0 +1,36 @@
+import shutil
+import glob
+import os
+
+
+import os
+
+def get_files(path):
+    for file in os.listdir(path):
+        if os.path.isfile(os.path.join(path, file)):
+            yield file
+
+
+dirs = [ f.path for f in os.scandir("./") if f.is_dir() ]
+
+
+
+for i in dirs:
+    if i == './export':
+        dirs.remove(i)
+
+
+for i in dirs:
+    files = get_files(i)
+
+    for file in files:
+        print(file)
+
+    pathOld = i + "/" + file
+
+    pathNew = './export/' + i[2:] + ".pak"
+
+
+    print(f"Old Path: {pathOld}, New Path: {pathNew}")
+    shutil.copyfile(pathOld, pathNew)
+    print("COPIED")
